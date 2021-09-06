1d6f9c106703912d6c40366803bdf7e44a6e3afe
darrenthamtoro <darrenthamtoro@gmail.com>
2021-09-06 14:19:01 -0700

    edited resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 
-function Resume(){
-    return(
-        <>
-        <a href="">Click Here to Download!!</a>
-        <ul> Technical Skills : 
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-        </ul>
-        </>
+function Resume() {
+    return (
+        <div className="container">
+            <section id="resume" className="page-section">
+                <h1>Resume </h1>
+
+                {/* <a href="">Click Here to Download!!</a> */}
+                <ul className="resume-info"> 
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                </ul>
+            </section>
+        </div>
     )
 }
 
-export default Resume; 
+export default Resume;
